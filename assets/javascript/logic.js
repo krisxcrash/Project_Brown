8e3a1f8972d77520563808ea3c032fa18bebbd5a
chrisjal <cjalallian@gmail.com>
2017-07-30 18:56:24 -0700

    fixed toggles, added comments, cleared forms on submit

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -23,20 +23,31 @@ $("#submit-button").on("click", function() {
 	distanceSearch = parseInt($("#input-distance").val());
 	startDate = $("#input-start").val();
 	endDate = $("#input-end").val();
+	// Hides form panel on submission
+	$(".toggle-form-container").slideUp("slow");
 
 	console.log(distanceSearch);
+
+	// Converts time from our calendar to UTC
 	var convertedStart = moment.utc(startDate).format();
 	var convertedEnd = moment.utc(endDate).format();
-	var queryURL = "https://www.eventbriteapi.com/v3/events/search/?token=PBMOFDBNG7TPGBUP7OSZ" + "&q=" + generalSearch + "&location.address=" + locationSearch + "&location.within=" + distanceSearch+"mi" + "&start_date.range_start=" + convertedStart + "&start_date.range_end=" + convertedEnd;
 
+	var queryURL = "https://www.eventbriteapi.com/v3/events/search/?token=PBMOFDBNG7TPGBUP7OSZ" + "&q=" + generalSearch + "&location.address=" + locationSearch + "&location.within=" + distanceSearch+"mi" + "&start_date.range_start=" + convertedStart + "&start_date.range_end=" + convertedEnd;
 	$.ajax({
         url: queryURL,
         method: "GET"
-      }).done(function(response) {
+    }).done(function(response) {
       	console.log(response);
-      });
+    });
+    // Clear form
+    $('form').trigger("reset");
+    // Toggles form by clicking "Search" panel header
+    $(".toggle-form").click(function(){
+		$(".toggle-form").slideToggle("slow");
+	});
 });
 
+// Calendar function for start/end dates
 $(function() {
 	$("#input-start").datepicker();
 	$("#input-end").datepicker();
