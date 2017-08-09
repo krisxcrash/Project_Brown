cedc29503180f3634be04ea046a8cb85559318d9
heeyoonchung <heeyc1@uci.edu>
2017-08-08 18:50:32 -0700

    Added pagination/expandable table of event results

---
+++ logic.js
@@ -0,0 +1,157 @@
+// Revisions:
+// 8/6 18:00 CJ - Updated current, simplified JS
+// 8/6 19:00 CJ - Added comments, fixed Null images, retooled to "slightly" fit search-results.html
+
+// 8/6 To DO: convert time values given to us, Re-implement form-toggling/hide on submit, search result pagination
+
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyDmBUkW-YBfdAME8Jo333BxYuj7FJ42K3M",
+	authDomain: "the-event-network.firebaseapp.com",
+	databaseURL: "https://the-event-network.firebaseio.com",
+	projectId: "the-event-network",
+	storageBucket: "the-event-network.appspot.com",
+	messagingSenderId: "388370003060"
+};
+firebase.initializeApp(config);
+
+var generalSearch = "";
+var categorySearch = "";
+var locationSearch = "";
+var distanceSearch = 0;
+var startDate = "";
+var endDate = "";
+
+// On submit button click, perform ajax call and main search/populating functions
+$("#submit-button").on("click", function() {
+	event.preventDefault();
+	generalSearch = $("#input-general").val().trim();
+	locationSearch = $("#input-location").val().trim();
+	distanceSearch = parseInt($(".input-distance").val());
+	startDate = $("#input-start").val();
+	endDate = $("#input-end").val();
+
+	// Hides form panel on submission -- unused as of 8/6. Needs re-implementation
+	// $(".toggle-form-container").slideUp("slow");
+
+	// Converts time from our calendar to UTC for queryURL
+	var convertedStart = moment.utc(startDate).format();
+	var convertedEnd = moment.utc(endDate).format();
+
+	var queryURL = "https://www.eventbriteapi.com/v3/events/search/?token=PBMOFDBNG7TPGBUP7OSZ" + "&q=" + generalSearch + "&location.address=" + locationSearch + "&location.within=" + distanceSearch+"mi" + "&start_date.range_start=" + convertedStart + "&start_date.range_end=" + convertedEnd + "&expand=organizer,venue";
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+		}).done(function(response) {
+			console.log(response);
+			var results = response.events;
+			console.log(response.events);
+			// loop through JSON results to create unique variables/storage/populating HTML
+			results.forEach(function(result) {
+				// Saving JSON object results to variables
+				var eventImageDiv = $("<img>");
+				// var eventImage = result.logo.url;
+				if (result.logo === null) {
+					var eventImage = "https://placehold.it/400x200";
+					eventImageDiv.attr("src", "https://placehold.it/400x200");
+				}
+				else {
+					var eventImage = result.logo.url;
+					eventImageDiv.attr("src", eventImage);
+				}
+				var eventTitle = result.name.text;
+				var eventDescription = result.description.text;
+				var startEvent = result.start.local;
+				var endEvent = result.end.local;
+				var venueName = result.venue.name;
+				var venueLat = parseInt(result.venue.latitude); // Unused
+				var venueLon = parseInt(result.venue.longitude); // Unused
+				var venueAddress = result.venue.address.localized_address_display;
+				var organizerName = result.organizer.name;
+
+				//creates dynamic div for populating results
+				var eventDiv = $("<div class='column event-list'>");
+				// When a result's div is clicked, that information is saved in sessionstorage to be passed onto event page
+				eventDiv.on("click", function() {
+					sessionStorage.setItem("title", eventTitle);
+					sessionStorage.setItem("description", eventDescription);
+					sessionStorage.setItem("eventstart", startEvent);
+					sessionStorage.setItem("eventend", endEvent);
+					sessionStorage.setItem("logo", eventImage);
+					sessionStorage.setItem("venue", venueName);
+					sessionStorage.setItem("latitude", venueLat);
+					sessionStorage.setItem("longitude", venueLon);
+					sessionStorage.setItem("address", venueAddress);
+					sessionStorage.setItem("organizer", organizerName);
+				})
+				eventDiv.append(eventImageDiv);
+				eventDiv.append("<p>" + eventTitle + "</p>");
+				// eventDiv.append("<p class= 'description'>" + eventDescription + "</p>");
+				eventDiv.append("<p class='event-time'>Start Time: </p><p>" + startEvent + "</p>");
+				eventDiv.append("<p class='event-time'>End Time: </p><p>" + endEvent + "</p>");
+				// eventDiv.append("<h5>Organizer: </h5><h6>" + organizerName + "</h6>");
+				eventDiv.append("<p class='event-time'>Location: </p><p>" + venueName + "</p>");
+				// eventDiv.append("<h5>Address: </h5><h6>" + venueAddress + "</h6>");
+				$(".search-results").prepend(eventDiv);
+			})
+		});
+    // Clear form
+    $('form').trigger("reset");
+    // Toggles form by clicking "Search" panel header -- Needs re-implementation
+ 	//    $(".toggle-form").click(function(){
+	// 	$(".toggle-form-container").slideDown("slow");
+	// });
+});
+
+$(function() {
+	var dateFormat = "mm/dd/yy",
+		from = $("#input-start")
+			.datepicker({
+				defaultDate: "+1w",
+				changeMonth: true,
+				numberOfMonths: 2
+		})
+		.on("change", function() {
+			to.datepicker("option", "minDate", getDate(this));
+		}),
+		to = $("#input-end").datepicker({
+			defaultDate: "+1w",
+			changeMonth: true,
+			numberOfMonths: 2
+		})
+		.on("change", function() {
+			from.datepicker("option", "maxDate", getDate(this));
+		});
+ 
+	function getDate(element) {
+		var date;
+		try {
+			date = $.datepicker.parseDate(dateFormat, element.value);
+		} catch(error) {
+			date = null;
+		}
+		return date;
+	}
+});
+
+
+// function mapCreator() {
+// 					map = google.maps.Map(document.getElementById('map'), {
+// 						center: {lat: venueLat, lng: venueLon},
+// 						zoom: 13
+// 					});
+// 				}
+
+// Upon clicking a respective result, expands the table containing the event results organized by title, location, start, end
+$(function() {
+    $("td[colspan=3]").find("p").hide();
+    $("table").click(function(event) {
+        event.stopPropagation();
+        var $target = $(event.target);
+        if ( $target.closest("td").attr("colspan") > 1 ) {
+            $target.slideUp();
+        } else {
+            $target.closest("tr").next().find("p").slideToggle();
+        }                    
+    });
+});
