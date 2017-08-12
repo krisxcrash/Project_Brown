7d6e75c5730594b040b3011bade3bd1321f628a0
chrisjal <cjalallian@gmail.com>
2017-08-11 20:53:49 -0700

    removed yelp code, cleaned some portions of code

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -52,7 +52,6 @@ $("#submit-button").on("click", function() {
 			results.forEach(function(result) {
 				// Saving JSON object results to variables
 				var eventImageDiv = $("<img class='event-image-toggled'>");
-				// var eventImage = result.logo.url;
 				if (result.logo === null) {
 					var eventImage = "https://placehold.it/400x200";
 					eventImageDiv.attr("src", "https://placehold.it/400x200");
@@ -60,6 +59,8 @@ $("#submit-button").on("click", function() {
 				else {
 					var eventImage = result.logo.url;
 					eventImageDiv.attr("src", eventImage).attr("href", "eventpage.html");
+					// originalLogo size/URL for use on results page
+					var originalLogo = result.logo.original.url;
 				}
 				var eventTitle = result.name.text;
 				var eventDescription = result.description.text;
@@ -92,6 +93,7 @@ $("#submit-button").on("click", function() {
 					localStorage.setItem("eventstart", startEventConverted);
 					localStorage.setItem("eventend", endEventConverted);
 					localStorage.setItem("logo", eventImage);
+					localStorage.setItem("biglogo", originalLogo), 
 					localStorage.setItem("venue", venueName);
 					localStorage.setItem("latitude", venueLat);
 					localStorage.setItem("longitude", venueLon);
@@ -140,41 +142,6 @@ $(function() {
 		return date;
 	}
 });
-
-// Yelp APi & Node.js
-
-	var q = "restaurants";
-	var loc = $(localStorage.getItem("address"));
-
-$(document).ready(function() {
-	var queryURL = 	"https://pure-savannah-62932.herokuapp.com/yelp/?q=" + q + "&location=" + loc + "&radius=5mi&open_now=true" 
-
-	$.ajax({
-		url: queryURL,
-		method: "GET"
-	}).done(function(response) {
-		var yelpResults = response.jsonBody.businesses;	
-
-		console.log(yelpResults);
-
-		for (var i = 0; i < yelpResults.length; i++) {
-			var distanceMiles = Math.round((yelpResults[i].distance * 0.000621371192)*100)/100;
-
-			console.log(yelpResults[i].location);
-
-			var yelpDiv = $("<div class='items'>")
-
-			var yelpData =  "<div class='image-resize-div'><img class='thumbnail image-resize' src='" + yelpResults[i].image_url + "' width='300'></div><h5><b>" + yelpResults[i].name + "</b></h5><p><b>Price:</b> " + yelpResults[i].price + "<br/><b>Rating:</b> " + yelpResults[i].rating + "/5<br/><b>Miles Away:</b> " + distanceMiles + "<br/><b>Phone:</b> " + yelpResults[i].display_phone +"</p><a href='" + yelpResults[i].url + "' class='button small expanded hollow yelp-link'>Take Me There</a>";
-
-			// + yelpResults[i].display_phone + "</td><td>" + yelpResults[i].location.display_address + "</td><td>" + distanceMiles +"</td>";
-
-			yelpDiv.append(yelpData);
-
-			$(".results").append(yelpDiv);
-		};
-	});
-});
-
 
 // function mapCreator() {
 // 					map = google.maps.Map(document.getElementById('map'), {
